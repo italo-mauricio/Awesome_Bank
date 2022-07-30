85c23000d07560c662b50e714a376ee8d87569dc
Italo Maurício <105960758+italomauricio1@users.noreply.github.com>
2022-07-30 20:21:55 -0300

    Criando módulo de depósito

--- saque.py
+++ saque.py
@@ -1,20 +1,51 @@
-def saque():
-    valor = int(input('Qual o valor você quer sacar da sua conta: '))
-    cedula = 50
-    total = valor
-    cedt = 0
+from cadacliente import *
+
+dici = diciclientes
+
+dicicaixa = {}
+def deposibanco():
+     print("=="*50)
+    print(''' 
+    | ---------------------  Bem vindos ao depósito! -------------------------- |
+    | ------- Se você está cadastrado no nosso sistema, poderá realizar seu depósito! ----------- |
+    | ===================================================================================== |
+            ''')
+    print("=="*50)
+
+
+
+def saquebanco():
+    print("=="*50)
+    print(''' 
+    | ---------------------  Bem vindos ao saque! -------------------------- |
+    | ------- Se você está cadastrado no nosso sistema, poderá realizar seu saque! ----------- |
+    | ===================================================================================== |
+            ''')
+    print("=="*50)
     while True:
-        if total >= cedula:
-            total -= cedula
-            cedt +=1
+        cadastro = input("Digite a sua senha: ")
+        if cadastro not in dici:
+            print("Usuário não encontrado!")
+            break
         else:
-            print(f'Total de {cedt} céduas de R${cedula}')
-            if cedula == 50:
-                cedula = 20
-            elif cedula == 20:
-                cedula = 10
-            elif cedula == 10:
-                cedula = 1
+            print("Cliente encontrado!")
+            valor = int(input('Qual o valor você quer sacar da sua conta: '))
+            cedula = 50
+            total = valor
             cedt = 0
-            if total == 0:
-                break
+            while True:
+                if total >= cedula:
+                    total -= cedula
+                    cedt +=1
+                else:
+                    print(f'Total de {cedt} céduas de R${cedula}')
+                    if cedula == 50:
+                        cedula = 20
+                    elif cedula == 20:
+                        cedula = 10
+                    elif cedula == 10:
+                        cedula = 1
+                    cedt = 0
+                    if total == 0:
+                        break
+      
