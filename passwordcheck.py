ed9a81030dcc6133d91c20c797911ff4d09fb591
Italo Mauricio <105960758+italo-mauricio@users.noreply.github.com>
2022-10-11 15:16:30 -0300

    Update passwordcheck.py

--- passwordcheck.py
+++ passwordcheck.py
@@ -9,7 +9,7 @@ def passw(password):
             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t','u',
             'v', 'x', 'y', 'z', '1','2','3','4','5','6','7','8',
             '9','0','A', 'B', 'C','D','E','F','G','H','I','J','K',
-            'L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z',]
+            'L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z','@', '_']
 
     # define password como vazio
     password = ""
