a8125f21cfe7a5a63aa7715cc3813e354f6fc8f3
italo <italogithub@gmail.com>
2022-10-15 02:50:25 -0300

    mudando idioma do projeto

--- main.py
+++ main.py
@@ -11,40 +11,40 @@ from emprestimo import *
 def menu():
     os.system("cls")
     while True:
-        opcao = ' '
-        hora_atual = datetime.now()
-        hora = hora_atual.strftime('%H:%M')
-        data = date.today()
+        option = ' '
+        time_now = datetime.now()
+        hour = time_now.strftime('%H:%M')
+        date = date.today()
         print(f'''                                           
         | ================================================================== |
-        |                         Banco Do Brasil                            |
+        |                       Revolution Bank                              |
         | ================================================================== |
-                           Data: {data} / Hora: {hora}
+                           Date: {date} / Time: {hour}
         | ================================================================== |
-        |                   Cadastrar clientes:    1                         |
-        |                   Saque em conta:        2                         |
-        |                   Deletar conta:         3                         |
-        |                   Consultar Cotação:     4                         |
-        |                   Empréstimos            5                         |
-        |                   Sair do Sistema:       0                         |    
+        |                   Register Users:        1                         |
+        |                   Account Withdrawal:    2                         |
+        |                   Delete Account:        3                         |
+        |                   Consult Quote:         4                         |
+        |                   Loans                  5                         |
+        |                   Exit:                  0                         |    
         | ================================================================== |
-        | ---------------   Obrigado pela preferência!   ------------------- |
+        | --------------   Thank you for your preference   ----------------- |
         |                          since 2022                                |
         ''')
-        opcao = input("Escolha a sua opção: ")
+        option = input("Choose your option: ")
 
-        if opcao == "1":
+        if option == "1":
             regcliente()
-        elif opcao == "2":
+        elif option == "2":
             saque.menusaque()
-        elif opcao == "3":
+        elif option == "3":
             dele.delusu()
-        elif opcao == "4":
+        elif option == "4":
             menucot()
-        elif opcao == "5":
+        elif option == "5":
             emprest()
-        elif opcao == "0":
-            print("Obrigado pela preferência!")
+        elif option == "0":
+            print("Thank you for your preference!")
             break
         else:
             print('Opção inválida!')
